3840c0a5aa53628e6231d18b0b92fc86683147b1
paulcenir003 <paul.cenir03@gmail.com>
2021-05-11 22:38:41 +0800

    widget update

--- src/app/builder/component/widgets.component.ts
+++ src/app/builder/component/widgets.component.ts
@@ -1,10 +1,32 @@
+import { RegionsService } from './../services/regions.service';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'widgets-root',
-  templateUrl: './widgets.component.html',
-  styleUrls: ['./widgets.component.less']
+    selector: 'widgets-root',
+    templateUrl: './widgets.component.html',
+    styleUrls: ['./widgets.component.less']
 })
 export class WidgetsComponent {
-  title = 'isd-builder';
+    customRegions;
+    constructor(
+        public RegionsService: RegionsService
+    ) {
+
+    }
+    ngOnInit() {
+        this.getCustomRegions();
+    }
+
+    getCustomRegions() {
+        this.RegionsService.getCustomRegions().subscribe(data => {
+            if (data) {
+                this.customRegions = Object.entries(data);
+                console.log(this.customRegions);
+            }
+        });
+    }
+
+    convertObjectToArray(object) {
+        return Object.entries(object)
+    }
 }
