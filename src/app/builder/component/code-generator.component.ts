a8d4b84383f5500bfff44b5f6eacf6a0668469b7
paulcenir003 <paul.cenir03@gmail.com>
2021-09-21 15:06:23 +0800

    code generator update

--- src/app/builder/component/code-generator.component.ts
+++ src/app/builder/component/code-generator.component.ts
@@ -1,10 +1,53 @@
+import { RegionsService } from './../services/regions.service';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'code-generator-root',
-  templateUrl: './code-generator.component.html',
-  styleUrls: ['./code-generator.component.less']
+    selector: 'code-generator-root',
+    templateUrl: './code-generator.component.html',
+    styleUrls: ['./code-generator.component.less']
 })
 export class CodeGeneratorComponent {
-  title = 'isd-builder';
+    title = 'isd-builder';
+    customRegions;
+    cloneCustomRegions;
+    sectionTagOpen = '<section>';
+    sectionTagClose = '</section>';
+
+    constructor(
+        public RegionsService: RegionsService,
+    ) {
+
+    }
+
+    ngOnInit() {
+        this.getCustomRegions();
+    }
+
+    getCustomRegions() {
+        this.RegionsService.getCustomRegions().subscribe(data => {
+            if (data) {
+                this.customRegions = Object.entries(data);
+                this.cloneCustomRegions = data;
+                console.log(this.cloneCustomRegions);
+            }
+        });
+    }
+
+
+    createOpenTagBuilder(tag, className, prefix, suffix ) {
+
+        if (prefix) {
+            className = prefix + className;
+        }
+
+        if (suffix) {
+            className = className + suffix;
+        }
+        
+        return '<'+tag+' class="'+className+'">';
+    }
+
+    createCloseTagBuilder(tag) {
+        return '</'+tag+'>';
+    }
 }
