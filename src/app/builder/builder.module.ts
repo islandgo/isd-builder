5e4b501ee5a7f47d0f00d47da82a45377e1b4e2a
epe <goliathdavid024@gmail.com>
2021-11-12 15:02:20 +0800

    function

--- src/app/builder/builder.module.ts
+++ src/app/builder/builder.module.ts
@@ -1,44 +1,45 @@
-import { HtmlGeneratorComponent } from './component/html-generator.component';
-import { CssGeneratorComponent } from './component/css-generator.component';
-import { CodeGeneratorComponent } from './component/code-generator.component';
-import { RegionsComponent } from './component/regions.component';
-import { WidgetsComponent } from './component/widgets.component';
-import { BuilderRoutes } from './config/builder.routes';
-import { NgModule } from '@angular/core';
-import { BuilderComponent } from './component/builder.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
-import { DragDropModule } from '@angular/cdk/drag-drop';
-import { RegionSectionModel } from './model/payment.model';
-import { ModalModule } from 'ngx-bootstrap/modal';
-import { JsGeneratorComponent } from './component/js-generator.component';
-import { CssMobileGeneratorComponent } from './component/css-mobile-generator.component';
-import { WidgetConverterComponent } from './component/widget-converter.component';
-import { PageSetUpComponent } from './component/page-setup.component';
-import { HeaderGeneratorComponent } from './component/header-generator.component';
-import { AppCommonModule } from '../common/common.module';
-import { SectionBackgroundComponent } from './component/widget-css-component/section-background/section-background.component';
-import { GridSectionComponent } from './component/widget-css-component/grid-section/grid-section.component';
-import { GalleryImageComponent } from './component/widget-html-component/gallery-image/gallery-image.component';
-import { GalleryImageCssComponent } from './component/widget-css-component/gallery-image-css/gallery-image-css.component';
-import { WidgetListComponent } from './component/widget-list.component';
-import { SimpleSlickComponent } from './component/widget-html-component/simple-slick/simple-slick.component';
-import { SimpleSlickJsComponent } from './component/widget-js-component/simple-slick-js/simple-slick-js.component';
-import { SimpleSlickCssComponent } from './component/widget-css-component/simple-slick-css/simple-slick-css.component';
-import { WidgetListJsComponent } from './component/widget-list-js.component';
-import { WidgetListCssComponent } from './component/widget-list-css.component';
-import { ElementPeekCssComponent } from './component/widget-css-component/element-peek-css/element-peek-css.component';
-import { ElementPeekJsComponent } from './component/widget-js-component/element-peek-js/element-peek-js.component';
-import { ElementPeekHtmlComponent } from './component/widget-html-component/element-peek-html/element-peek-html.component';
-import { ContactFormComponent } from './component/widget-html-component/contact-form/contact-form.component';
-import { ContactFormCssComponent } from './component/widget-css-component/contact-form-css/contact-form-css.component';
-import { SiteTitleComponent } from './component/widget-html-component/site-title/site-title.component';
-import { QuickSearchComponent } from './component/widget-html-component/quick-search/quick-search.component';
-import { QuickSearchCssComponent } from './component/widget-css-component/quick-search-css/quick-search-css.component';
-import { WidgetListMobileCssComponent } from './component/widget-list-mobile-css.component';
-import { QuickSearchMobileCssComponent } from './component/widget-css-mobile-component/quick-search-css/quick-search-mobile-css.component';
-import { IntroVideoComponent } from './component/widget-html-component/intro-video/intro-video.component';
-import { QuickSearchJsComponent } from './component/widget-js-component/quick-search-js/quick-search-js.component';
+import { HtmlGeneratorComponent } from "./component/html-generator.component";
+import { CssGeneratorComponent } from "./component/css-generator.component";
+import { CodeGeneratorComponent } from "./component/code-generator.component";
+import { RegionsComponent } from "./component/regions.component";
+import { WidgetsComponent } from "./component/widgets.component";
+import { BuilderRoutes } from "./config/builder.routes";
+import { NgModule } from "@angular/core";
+import { BuilderComponent } from "./component/builder.component";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
+import { DragDropModule } from "@angular/cdk/drag-drop";
+import { RegionSectionModel } from "./model/payment.model";
+import { ModalModule } from "ngx-bootstrap/modal";
+import { JsGeneratorComponent } from "./component/js-generator.component";
+import { CssMobileGeneratorComponent } from "./component/css-mobile-generator.component";
+import { WidgetConverterComponent } from "./component/widget-converter.component";
+import { PageSetUpComponent } from "./component/page-setup.component";
+import { HeaderGeneratorComponent } from "./component/header-generator.component";
+import { AppCommonModule } from "../common/common.module";
+import { SectionBackgroundComponent } from "./component/widget-css-component/section-background/section-background.component";
+import { GridSectionComponent } from "./component/widget-css-component/grid-section/grid-section.component";
+import { GalleryImageComponent } from "./component/widget-html-component/gallery-image/gallery-image.component";
+import { GalleryImageCssComponent } from "./component/widget-css-component/gallery-image-css/gallery-image-css.component";
+import { WidgetListComponent } from "./component/widget-list.component";
+import { SimpleSlickComponent } from "./component/widget-html-component/simple-slick/simple-slick.component";
+import { SimpleSlickJsComponent } from "./component/widget-js-component/simple-slick-js/simple-slick-js.component";
+import { SimpleSlickCssComponent } from "./component/widget-css-component/simple-slick-css/simple-slick-css.component";
+import { WidgetListJsComponent } from "./component/widget-list-js.component";
+import { WidgetListCssComponent } from "./component/widget-list-css.component";
+import { ElementPeekCssComponent } from "./component/widget-css-component/element-peek-css/element-peek-css.component";
+import { ElementPeekJsComponent } from "./component/widget-js-component/element-peek-js/element-peek-js.component";
+import { ElementPeekHtmlComponent } from "./component/widget-html-component/element-peek-html/element-peek-html.component";
+import { ContactFormComponent } from "./component/widget-html-component/contact-form/contact-form.component";
+import { ContactFormCssComponent } from "./component/widget-css-component/contact-form-css/contact-form-css.component";
+import { SiteTitleComponent } from "./component/widget-html-component/site-title/site-title.component";
+import { QuickSearchComponent } from "./component/widget-html-component/quick-search/quick-search.component";
+import { QuickSearchCssComponent } from "./component/widget-css-component/quick-search-css/quick-search-css.component";
+import { WidgetListMobileCssComponent } from "./component/widget-list-mobile-css.component";
+import { QuickSearchMobileCssComponent } from "./component/widget-css-mobile-component/quick-search-css/quick-search-mobile-css.component";
+import { IntroVideoComponent } from "./component/widget-html-component/intro-video/intro-video.component";
+import { QuickSearchJsComponent } from "./component/widget-js-component/quick-search-js/quick-search-js.component";
+import { FunctionGeneratorComponent } from "./component/function-generator.component";
 
 @NgModule({
   declarations: [
@@ -74,7 +75,8 @@ import { QuickSearchJsComponent } from './component/widget-js-component/quick-se
     QuickSearchCssComponent,
     QuickSearchMobileCssComponent,
     IntroVideoComponent,
-    QuickSearchJsComponent
+    QuickSearchJsComponent,
+    FunctionGeneratorComponent,
   ],
   imports: [
     BuilderRoutes,
@@ -83,10 +85,8 @@ import { QuickSearchJsComponent } from './component/widget-js-component/quick-se
     ReactiveFormsModule,
     DragDropModule,
     AppCommonModule,
-    CommonModule
+    CommonModule,
   ],
-  providers: [
-    RegionSectionModel
-  ]
+  providers: [RegionSectionModel],
 })
-export class AppBuilderModule { }
+export class AppBuilderModule {}
---
+++ src/app/builder/component/function-generator.component.ts
@@ -0,0 +1,34 @@
+import { PageService } from "./../services/page.service";
+import { RegionsService } from "../services/regions.service";
+import { Component, Input } from "@angular/core";
+
+@Component({
+  selector: "function-generator",
+  templateUrl: "./function-generator.component.html",
+  styleUrls: ["./function-generator.component.less"],
+})
+export class FunctionGeneratorComponent {
+  @Input() public customRegions;
+  @Input() public pageSettings;
+  constructor(RegionsService: RegionsService, PageService: PageService) {}
+
+  ngOnInit() {}
+
+  convertToCamelCase(name) {
+    name = name.replace("-", " ");
+    // name = this.convertToMethodName(name);
+    return this.titleCase(name);
+  }
+
+  titleCase(str) {
+    var splitStr = str.toLowerCase().split(" ");
+    for (var i = 0; i < splitStr.length; i++) {
+      // You do not need to check if i is larger than splitStr length, as your for does that for you
+      // Assign it back to the array
+      splitStr[i] =
+        splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+    }
+    // Directly return the joined string
+    return splitStr.join(" ");
+  }
+}
