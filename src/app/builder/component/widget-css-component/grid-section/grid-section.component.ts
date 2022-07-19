b7e8a2602337f5b1150f7c0d9c16a3965c28127e
johndoe <johndoe@gmail.com>
2022-07-19 20:07:10 +0800

    First

--- src/app/builder/component/widget-css-component/grid-section/grid-section.component.ts
+++ src/app/builder/component/widget-css-component/grid-section/grid-section.component.ts
@@ -1,21 +1,19 @@
-import { RegionsService } from '../../../services/regions.service';
-import { PageService } from '../../../services/page.service';
-import { Component, Input, OnInit } from '@angular/core';
+import { RegionsService } from "../../../services/regions.service";
+import { PageService } from "../../../services/page.service";
+import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-grid-section',
-  templateUrl: './grid-section.component.html',
-  styleUrls: ['./grid-section.component.less']
+  selector: "app-grid-section",
+  templateUrl: "./grid-section.component.html",
+  styleUrls: ["./grid-section.component.less"],
 })
 export class GridSectionComponent implements OnInit {
   @Input() public region;
+
   constructor(
     public PageService: PageService,
     public RegionsService: RegionsService
-  ) { }
+  ) {}
 
-  ngOnInit() {
-
-  }
-
+  ngOnInit() {}
 }
