dfe6d393d8fa6024ed2bd7791e471b9bd308b18a
johndoe <johndoe@gmail.com>
2022-08-13 15:33:56 +0800

    asdf

--- src/app/builder/component/widget-css-component/simple-slick-css/simple-slick-css.component.ts
+++ src/app/builder/component/widget-css-component/simple-slick-css/simple-slick-css.component.ts
@@ -9,11 +9,35 @@ import { RegionsService } from "src/app/builder/services/regions.service";
 export class SimpleSlickCssComponent implements OnInit {
   @Input() public widget;
   @Input() public regionData;
+  slickVideo: any;
+  grayscale: any;
+  contentPosition: any;
+  contentType: any;
+  hoverWithImg: any;
+  hoverWithImgLogo: any;
+  hoverWithBorder: any;
+  gradientBottomOverlay: any;
+  dots: any;
+  arrows: any;
 
   constructor(public RegionsService: RegionsService) {}
 
   ngOnInit() {
     if (this.widget[1]["name"] == "simple-slick") {
+      this.slickVideo = this.widget[1]["settings"]["slickVideo"]["value"];
+      this.grayscale = this.widget[1]["settings"]["grayScaleOnImage"]["value"];
+      this.contentPosition =
+        this.widget[1]["settings"]["contentPosition"]["value"];
+      this.contentType = this.widget[1]["settings"]["contentType"]["value"];
+      this.hoverWithImg = this.widget[1]["settings"]["hoverWithImg"]["value"];
+      this.hoverWithImgLogo =
+        this.widget[1]["settings"]["hoverWithImgLogo"]["value"];
+      this.hoverWithBorder =
+        this.widget[1]["settings"]["hoverWithBorder"]["value"];
+      this.gradientBottomOverlay =
+        this.widget[1]["settings"]["gradientBottomOverlay"]["value"];
+      this.dots = this.widget[1]["settings"]["dots"]["value"];
+      this.arrows = this.widget[1]["settings"]["arrows"]["value"];
     }
   }
 }
