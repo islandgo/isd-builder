30c06993cd6bd25303a5f21a6e8d7810148dd8b6
johndoe <johndoe@gmail.com>
2022-08-22 16:57:41 +0800

    gallery image update

--- src/app/builder/component/widget-html-component/gallery-image/gallery-image.component.ts
+++ src/app/builder/component/widget-html-component/gallery-image/gallery-image.component.ts
@@ -14,6 +14,11 @@ export class GalleryImageComponent implements OnInit {
   widgetSettings;
   galleryItem: any;
   imageType: any;
+  imageWidth: any;
+  imageHeight: any;
+  contentType: any;
+  contentPosition: any;
+  hoverWithImgLogo: any;
   constructor(public RegionsService: RegionsService) {}
 
   ngOnInit() {
@@ -22,6 +27,13 @@ export class GalleryImageComponent implements OnInit {
       this.widgetSettings = this.widget[1]["settings"];
       this.galleryItem = +this.widget[1]["settings"]["number_of_item"]["value"];
       this.imageType = this.widget[1]["settings"]["imageType"]["value"];
+      this.imageWidth = this.widget[1]["settings"]["image_width"]["value"];
+      this.imageHeight = this.widget[1]["settings"]["image_height"]["value"];
+      this.contentType = this.widget[1]["settings"]["contentType"]["value"];
+      this.contentPosition =
+        this.widget[1]["settings"]["contentPosition"]["value"];
+      this.hoverWithImgLogo =
+        this.widget[1]["settings"]["hoverWithImgLogo"]["value"];
     }
   }
 }
